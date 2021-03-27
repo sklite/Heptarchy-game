035b19d9cd60b654e1fe71ecdf4153e9fdec2808
Alexey Bakin <alexey.b@ecomsolutions.net>
2021-03-28 00:24:12 +0300

    Initial commit

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 botLeft, topRight;
+
+    float borderEpsilon = 0.2f;
+
+  //  GameObject ui;
+    List<Func<Vector3, bool>> bordersCheck;
+
+
+
+    public CameraController()
+    {
+        bordersCheck = new List<Func<Vector3, bool>>
+        {
+            direction => direction.x > 0 && topRight.x > MapSettings.MapSize.x - borderEpsilon,
+            direction => direction.x < 0 && botLeft.x < -MapSettings.MapSize.x + borderEpsilon,
+            direction => direction.y > 0 && topRight.y > MapSettings.MapSize.y - borderEpsilon,
+            direction => direction.y < 0 && botLeft.y < -MapSettings.MapSize.y + borderEpsilon
+        };
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+     //   ui = GameObject.Find("Controls");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        botLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        //print position of mouse
+        var mousex = (Input.mousePosition.x);
+        var mousey = (Input.mousePosition.y);
+        var mouseposition = Camera.main.ScreenToWorldPoint(new Vector3(mousex, mousey, 0));
+      //  print(mouseposition);
+
+    }
+
+
+    public void Move(Vector3 direction)
+    {
+        if (bordersCheck.Any(func => func(direction)))
+            return;
+
+        transform.Translate(direction * Time.deltaTime * 2);
+    }
+
+
+    public float Right
+    {
+        get { return transform.position.x + Camera.main.orthographicSize; }
+    }
+}
---
+++ Assets/Scripts/Camera/ScrollerScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollerScript : MonoBehaviour
+{
+    public float Speed;
+    public ScrollerDirections Direction;
+
+    bool _isPressed;
+    Vector3 _direction;
+    CameraController _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GameObject.Find("Main Camera").GetComponent<CameraController>();
+        SetDirection(Direction);
+        //ui = GameObject.Find("UI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var boxCollider = GetComponent<BoxCollider2D>();
+            var mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePositionInWorld.z = 0;
+            if (boxCollider.bounds.Contains(mousePositionInWorld))
+            {
+                _isPressed = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+            _isPressed = false;
+
+        UpdatePressing();
+    }
+
+    void UpdatePressing()
+    {
+        if (_isPressed)
+        {
+            Press();
+        }
+    }
+
+    public void SetDirection(ScrollerDirections dir)
+    {
+        switch (dir)
+        {
+            case ScrollerDirections.Up:
+                _direction = new Vector3(0, Speed, 0);
+                break;
+            case ScrollerDirections.Down:
+                _direction = new Vector3(0, -Speed, 0);
+                break;
+            case ScrollerDirections.Left:
+                _direction = new Vector3(-Speed, 0, 0);
+                break;
+            case ScrollerDirections.Right:
+                _direction = new Vector3(Speed, 0, 0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Press()
+    {
+        _camera.Move(_direction);
+    }
+
+    
+}
+
+public enum ScrollerDirections
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
