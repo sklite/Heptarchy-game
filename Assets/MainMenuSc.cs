b1265686b35bea15be239cda0048aba841070e1e
Alexey Bakin <sk-lite@ya.ru>
2021-05-04 02:06:09 +0300

    Play with menu

--- Assets/MainMenuSc.cs
+++ Assets/MainMenuSc.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuSc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
+    //}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
     }
 }
